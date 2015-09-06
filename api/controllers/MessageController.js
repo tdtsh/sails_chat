ee82a268bb9fae18663559e6e0e5c07b7a25682d
tdtsh <systemsfailing@gmail.com>
2015-09-06 14:39:14 +0900

    pub/sub

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -9,20 +9,19 @@ module.exports = {
 
 	find: function(req, res) {
 		console.log("GET /message");
-
 		Message.find().exec(function(err, messages) {
+			Message.subscribe(req.socket);
 			res.json(messages);
 		});
 	},
 
 	create: function(req, res) {
 		console.log("POST /message");
-
 		var text = req.param('text');
-
 		Message.create({
 			text: text
 		}).exec(function(err, message) {
+			Message.publishCreate(message);
 			res.json(message);
 		});
 	}
